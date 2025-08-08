1238df6d74d530c4862098efff285baf4c1d3c39
Chiobject <72055341+chiobject@users.noreply.github.com>
2025-08-08 16:23:05 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String str = bf.readLine();
+        System.out.println(str.length());
+    }
+}
+

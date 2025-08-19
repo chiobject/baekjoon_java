a955243c8f6692dd26a3669caf9c1bfbdccc1725
Chiobject <72055341+chiobject@users.noreply.github.com>
2025-08-19 14:51:35 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 328 ms, Memory: 20816 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int input_n = Integer.parseInt(input[0]);
+        int input_m = Integer.parseInt(input[1]);
+        
+        int[][] A_arr = new int[input_n][input_m];
+        int[][] B_arr = new int[input_n][input_m];
+
+        for(int i = 0; i < input_n; i++){
+            String[] A_arr_line = br.readLine().split(" ");
+            for(int j = 0; j < input_m; j++){
+                A_arr[i][j] = Integer.parseInt(A_arr_line[j]);
+            }
+        }
+
+        for(int i = 0; i < input_n; i++){
+            String[] B_arr_line = br.readLine().split(" ");
+            for(int j = 0; j < input_m; j++){
+                B_arr[i][j] = Integer.parseInt(B_arr_line[j]);
+            }
+        }
+
+        for(int i = 0; i < input_n; i++){
+            for(int j = 0; j < input_m; j++){
+                System.out.print((A_arr[i][j] + B_arr[i][j]));
+                if(j < input_m - 1){
+                    System.out.print(" ");
+                }
+            }
+            if(i < input_n - 1){
+                System.out.println("");
+            }
+        }
+    }
+}
